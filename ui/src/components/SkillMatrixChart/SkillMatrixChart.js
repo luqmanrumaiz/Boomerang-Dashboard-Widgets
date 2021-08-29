e1f01ed0c024ca00d78614ce60d04c0d4e2827fc
unknown <adrian.20191176@iit.ac.lk>
2021-08-30 02:25:01 +0530

    SkillMatrix Chart

--- ui/src/components/SkillMatrixChart/SkillMatrixChart.js
+++ ui/src/components/SkillMatrixChart/SkillMatrixChart.js
@@ -1,6 +1,78 @@
 import React from "react";
 import "./SkillMatrixChart.css";
+import {ResponsiveBar } from "@nivo/bar";
 
-export default function SkillMatrixChart() {
-  return <>Skill Matrix Chart</>;
+
+var dep = ["BA/PMO","Corporate Communication","Development","Devops","HR","Managerial","Quality Assurance","UI/UX"];
+var male = [4,5,3,4,1,6,4,7]
+var female = [3,4,5,7,5,3,2,4]
+
+const chartData = () =>
+{
+  var arr = [];
+  for(var i=0; i<dep.length;i++){
+    let temp = {};
+    temp.Department = dep[i];
+    temp.Male = male[i];
+    temp.Female = female[i];
+    arr.push(temp);
+
+  }
+  return arr;
+}
+
+export default function TurnoverChart() {
+  return (
+    <ResponsiveBar
+        data={chartData()}
+        keys={[ 'Male', 'Female']}
+        indexBy="Department"
+        margin={{ right: 130, bottom: 50, left: 150 }}
+        padding={0.2}
+        groupMode="grouped"
+        layout="horizontal"
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        valueFormat={{ format: '', enabled: false }}
+        colors={['#4DB3B3',"#E64A45"]}
+        
+
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            tickSize: 5,
+            tickRotation: 0,
+            legend: 'No of Employees',
+            legendPosition: 'middle',
+            legendOffset: 32
+        }}
+        
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        legends={[
+            {
+                dataFrom: 'keys',
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: 'left-to-right',
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+  );
 }
