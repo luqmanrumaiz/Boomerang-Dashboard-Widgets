5d77a79dddedea18b4a6f5caac95bc7971bc650e
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-08-30 19:23:27 +0530

    Integrated Shavini's Age Group into the React Project and a minor Change
    Used the Age Group Chart's Theme into the Skill Matrix Chart

--- ui/src/components/AgeGroupChart/AgeGroupChart.js
+++ ui/src/components/AgeGroupChart/AgeGroupChart.js
@@ -1,6 +1,142 @@
 import React from "react";
 import "./AgeGroupChart.css";
+import { ResponsiveBar } from "@nivo/bar";
+
+const data = [
+  {
+    Age: "0-18",
+    Female: 0,
+    Male: 1,
+  },
+  {
+    Age: "19-24",
+    Female: 3,
+    Male: 7,
+  },
+  {
+    Age: "25-29",
+    Female: 3,
+    Male: 3,
+  },
+  {
+    Age: "30-34",
+    Female: 10,
+    Male: 25,
+  },
+  {
+    Age: "35-39",
+    Female: 7,
+    Male: 30,
+  },
+  {
+    Age: "40-44",
+    Female: 5,
+    Male: 14,
+  },
+  {
+    Age: "45-49",
+    Female: 2,
+    Male: 7,
+  },
+  {
+    Age: "50-54",
+    Female: 1,
+    Male: 1,
+  },
+  {
+    Age: "55-59",
+    Female: 0,
+    Male: 0,
+  },
+  {
+    Age: "60-64",
+    Female: 0,
+    Male: 0,
+  },
+  {
+    Age: "65-100",
+    Female: 0,
+    Male: 0,
+  },
+];
+
+const config = {
+  keys: ["Female", "Male"],
+
+  margin: {
+    top: 50,
+    right: 130,
+    bottom: 50,
+    left: 60,
+  },
+
+  axisBottom: {
+    tickSize: 5,
+    tickPadding: 5,
+    tickRotation: 0,
+    legend: "Age",
+    legendPosition: "middle",
+    legendOffset: 32,
+  },
+  axisLeft: {
+    tickSize: 5,
+    tickPadding: 5,
+    tickRotation: 0,
+    legend: "Head Count",
+    legendPosition: "middle",
+    legendOffset: -40,
+  },
+  legends: [
+    {
+      dataFrom: "keys",
+      anchor: "bottom-right",
+      direction: "column",
+      justify: false,
+      translateX: 120,
+      translateY: 0,
+      itemsSpacing: 2,
+      itemWidth: 100,
+      itemHeight: 20,
+      itemDirection: "left-to-right",
+      itemOpacity: 0.85,
+      symbolSize: 20,
+      effects: [
+        {
+          on: "hover",
+          style: {
+            itemOpacity: 1,
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const theme = { fontFamily: "Poppins" };
 
 export default function AgeGroupChart() {
-  return <>Age Group Chart</>;
+  return (
+    <ResponsiveBar
+      data={data}
+      keys={config.keys}
+      indexBy="Age"
+      margin={config.margin}
+      padding={0.3}
+      colors={["#ff705e", "#2581bb"]}
+      theme={theme}
+      defs={config.defs}
+      fill={config.fill}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={config.axisBottom}
+      axisLeft={config.axisLeft}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor="inherit:darker(1.6)"
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+      legends={config.legends}
+    />
+  );
 }
--- ui/src/components/SkillMatrixChart/SkillMatrixChart.js
+++ ui/src/components/SkillMatrixChart/SkillMatrixChart.js
@@ -1,80 +1,84 @@
 import React from "react";
 import "./SkillMatrixChart.css";
-import {ResponsiveBar } from "@nivo/bar";
+import { ResponsiveBar } from "@nivo/bar";
 
+var dep = [
+  "BA/PMO",
+  "Corporate Communication",
+  "Development",
+  "Devops",
+  "HR",
+  "Managerial",
+  "Quality Assurance",
+  "UI/UX",
+];
+var male = [4, 5, 3, 4, 1, 6, 4, 7];
+var female = [3, 4, 5, 7, 5, 3, 2, 4];
 
-var dep = ["BA/PMO","Corporate Communication","Development","Devops","HR","Managerial","Quality Assurance","UI/UX"];
-var male = [4,5,3,4,1,6,4,7]
-var female = [3,4,5,7,5,3,2,4]
-
-const chartData = () =>
-{
+const chartData = () => {
   var arr = [];
-  for(var i=0; i<dep.length;i++){
+  for (var i = 0; i < dep.length; i++) {
     let temp = {};
     temp.Department = dep[i];
     temp.Male = male[i];
     temp.Female = female[i];
     arr.push(temp);
-
   }
   return arr;
-}
+};
 
-const theme = {fontFamily:"Poppins"};
+const theme = { fontFamily: "Poppins" };
 
 export default function TurnoverChart() {
   return (
     <ResponsiveBar
-        data={chartData()}
-        keys={[ 'Male', 'Female']}
-        indexBy="Department"
-        margin={{ right: 130, bottom: 50, left: 170}}
-        padding={0.2}
-        groupMode="grouped"
-        layout="horizontal"
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        valueFormat={{ format: '', enabled: false }}
-        colors={['#4DB3B3',"#E64A45"]}
-        theme = {theme}
-
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 5,
-            tickRotation: 0,
-            legend: 'No of Employees',
-            legendPosition: 'middle',
-            legendOffset: 32
-        }}
-        
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        legends={[
+      data={chartData()}
+      keys={["Male", "Female"]}
+      indexBy="Department"
+      margin={{ right: 130, bottom: 50, left: 170 }}
+      padding={0.2}
+      groupMode="grouped"
+      layout="horizontal"
+      valueScale={{ type: "linear" }}
+      indexScale={{ type: "band", round: true }}
+      valueFormat={{ format: "", enabled: false }}
+      colors={["#ff705e", "#2581bb"]}
+      theme={theme}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 5,
+        tickRotation: 0,
+        legend: "No of Employees",
+        legendPosition: "middle",
+        legendOffset: 32,
+      }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      legends={[
+        {
+          dataFrom: "keys",
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: "left-to-right",
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
             {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
+              on: "hover",
+              style: {
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
     />
   );
 }
